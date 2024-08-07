527a392d9d6d9646bb27ac558321c9d0c851c2b6
Dr. Code <dr.code.yt@gmail.com>
2024-08-07 12:01:57 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "dr-code@google.com"
+var accountPassword = "12345"
+accountCity = "Islamabad"
+let accountState
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "23122"
+accountCity = "Lahore"
+
+//console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as per the newer standard
+
+// alert(3+3) // we are using node-js not browser
+
+
+// Primitive datatypes
+/* 
+number
+bigint
+string
+boolean
+null
+undefined
+symbol
+*/
+
+// object
+
+console.log(typeof "Usman")
+console.log(typeof Usman)
+console.log(typeof 23)
+console.log(typeof 289034789237948723894789273472890347892379487238947892734728903478923794872389478927347)
+console.log(typeof 23.343)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
